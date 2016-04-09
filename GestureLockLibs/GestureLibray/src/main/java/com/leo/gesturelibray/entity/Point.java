563743732ec41331c64c4c4a7b165aa2e584d3e1
leo <leo@ganguo.hk>
2016-04-10 01:13:06 +0800

    删除用不到类

--- GestureLockLibs/GestureLibray/src/main/java/com/leo/gesturelibray/entity/Point.java
+++ GestureLockLibs/GestureLibray/src/main/java/com/leo/gesturelibray/entity/Point.java
@@ -5,8 +5,8 @@ package com.leo.gesturelibray.entity;
  */
 public class Point {
     public static int STATE_NORMAL = 0; // 未选中
-    public static int STATE_CHECK = 1; // 选中 e
-    public static int STATE_CHECK_ERROR = 2; // 已经选中,但是输错误
+    public static int STATE_CHECK = 1; // 选中
+    public static int STATE_CHECK_ERROR = 2; // 输入错误
 
     public float x;
     public float y;
